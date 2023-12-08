ab9734583cd72b0a69221e981f29bce6c53276f9
tomkjose <tomjosekunnumpurath@gmail.com>
2023-12-08 22:31:31 +0530

    404 page added

--- src/pages/Fof.jsx
+++ src/pages/Fof.jsx
@@ -1,7 +1,15 @@
 import React from "react";
-
+import "../styles/fof.css";
+import FofImage from "../assets/images/404.gif";
 function Fof() {
-  return <div>Fof</div>;
+  return (
+    <div className="fof">
+      <div className="fof__card">
+        <img src={FofImage} alt="404" />
+        <button className="form__button">Home</button>
+      </div>
+    </div>
+  );
 }
 
 export default Fof;
